835c5646423df84c0abe13fa0b8d4c9140766dfa
unknown <al.ovchinkin@gmail.com>
2018-07-12 19:32:35 +0300

    reCaptcha added

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
     server : 'localhost',
     db     : 'lingvoric'
   },
-  rootVideoDir: process.cwd() + '/../PUBLIC/video-data/'
+  rootVideoDir: process.cwd() + '/../PUBLIC/video-data/',
+  reCaptchaUrl: 'https://www.google.com/recaptcha/api/siteverify'
 };
--- route-handlers/post-register-user.js
+++ route-handlers/post-register-user.js
@@ -1,9 +1,41 @@
 'use strict'
 
 const logger = require('../lib/logger');
+const queryString = require('query-string');
+const secretKeys = require('../secret-keys');
+const request = require('request');
+const config = require('../config');
 
 module.exports = function(req, res, next) {
   logger.info('handled route: POST register-user');
 
-  res.status(200).send('user registration - done');
+  const reCaptcha = req.body["g-recaptcha-response"];
+
+  if (reCaptcha) {
+    const params = queryString.stringify({
+      'secret'   : secretKeys.reCaptchaKey,
+      'response' : 'g-recaptcha-response',
+      'remoteip' : req.connection.remoteAddress 
+    });
+
+    const verificationUrl = config.reCaptchaUrl + '?' + params;
+
+    request(verificationUrl, function (error, response, body) {
+      if (error) return next(error);
+
+      const result = JSON.parse(body);
+
+      if(!result.success) {
+        res.status(403).send('reCaptcha verification failed');
+        logger.info(`reCaptcha verification failed, ip: ${req.connection.remoteAddress}`);
+        return;
+      }
+
+      res.status(200).send('user registration - done');
+    });
+
+    return;
+  }
+
+  res.status(200).send('error');
 };
