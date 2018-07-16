9fe6b5aa658c8d1b36447af25429b61a0a88e6b1
unknown <al.ovchinkin@gmail.com>
2018-07-16 12:40:01 +0300

    refactoring styles

--- scripts/frontend/cookie-agreement.js
+++ scripts/frontend/cookie-agreement.js
@@ -1,3 +1,13 @@
 (function() {
-  console.log(document.cookie);
+  var cookieAgreement = window.Cookies('cookie-agreement');
+
+  if(!cookieAgreement) {
+    $('#cookie-nav').removeClass('d-none');
+  }
+
+  $('#cookie-btn')
+    .on('click', function() {
+      window.Cookies('cookie-agreement', 'true', { expires: 364 });
+      $('#cookie-nav').addClass('d-none');
+    });
 })();
